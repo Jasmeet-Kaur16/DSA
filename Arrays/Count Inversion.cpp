3ab05a498c95bfe6793df51b38f23efa82f24984
Jasmeet-Kaur16 <92389678+Jasmeet-Kaur16@users.noreply.github.com>
2024-06-07 14:04:23 +0530

    Create Count Inversion.cpp

---
+++ Arrays/Count Inversion.cpp
@@ -0,0 +1,77 @@
+//BRUTE
+#include <bits/stdc++.h> 
+long long getInversions(long long *arr, int n){
+    
+    long long count=0;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
+//OPTIMAL ----MERGE SORT
+#include <bits/stdc++.h>
+
+void merge(long long *arr, int low, int mid, int high, long long &count)
+{
+    int n = high - low + 1;
+    vector<long long> temp(n);
+    int left = low;
+    int right = mid + 1;
+    int k = 0;
+
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp[k++] = arr[left++];
+        }
+        else
+        {
+            count += (mid - left + 1);
+            temp[k++] = arr[right++];
+        }
+    }
+
+    while (left <= mid)
+    {
+        temp[k++] = arr[left++];
+    }
+
+    while (right <= high)
+    {
+        temp[k++] = arr[right++];
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        arr[low + i] = temp[i];
+    }
+}
+
+void merge_sort(long long *arr, int low, int high, long long &count)
+{
+    if (low < high)
+    {
+        int mid = low + (high - low) / 2;
+        merge_sort(arr, low, mid, count);
+        merge_sort(arr, mid + 1, high, count);
+        merge(arr, low, mid, high, count);
+    }
+}
+
+long long getInversions(long long *arr, int n)
+{
+    long long count = 0;
+    merge_sort(arr, 0, n - 1, count);
+    return count;
+}
